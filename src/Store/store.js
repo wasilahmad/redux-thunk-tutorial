4a236d25124f07c4cbf20a5906e4f7ebe324c07f
wasil-sayyed <wasil.sayyed@isobar.com>
2020-03-31 20:18:47 +0530

    adding comment from personel git account

--- src/Store/store.js
+++ src/Store/store.js
@@ -6,6 +6,7 @@ import logger from 'redux-logger';
 // middleware dependencies goes here
 const middleWare = [thunk, logger];
 
+// central store to handle data
 const store = createStore(rootReducer, applyMiddleware(...middleWare));
 
 export default store;
