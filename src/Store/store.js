d388882c4b78714966e2efe8b821834405e8a367
wasil-sayyed <wasil.sayyed@isobar.com>
2020-03-31 19:09:33 +0530

    informative comment added

--- src/Store/store.js
+++ src/Store/store.js
@@ -3,6 +3,7 @@ import rootReducer from '../Reducer/root-reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+// middleware dependencies goes here
 const middleWare = [thunk, logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middleWare));
